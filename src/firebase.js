3214664d5cad23e8f35f48b24b71c0317f27a65c
IslandBoyDev25 <chezling.stoddard@gmail.com>
2022-01-06 23:58:34 -0400

    add nabar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD05aMuxHn1Xa5_Pfeu7vwV-YBMVA90LXU",
+  authDomain: "clone-a2f53.firebaseapp.com",
+  projectId: "clone-a2f53",
+  storageBucket: "clone-a2f53.appspot.com",
+  messagingSenderId: "535913700546",
+  appId: "1:535913700546:web:d347a8f51f63010c26ef5b",
+};
+
+export default firebaseConfig;
