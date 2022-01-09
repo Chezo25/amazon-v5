8cf2904be19b9cc9da2126c3b5d95ba38e5f5100
IslandBoyDev25 <chezling.stoddard@gmail.com>
2022-01-09 00:51:24 -0400

    add login

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     //BEM
     <Router>
       <div className="app">
-        <Header />
-
+        <Header exact path="/login" element={((<Home />), (<Checkout />))} />
         <Routes>
+          <Route exact path="/login" element={<Login />} />
+
           <Route exact path="/checkout" element={<Checkout />} />
           <Route exact path="/" element={<Home />} />
         </Routes>
