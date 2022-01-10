a37e7571071624f10cb8c69600df321ed01b045e
IslandBoyDev25 <chezling.stoddard@gmail.com>
2022-01-09 21:39:47 -0400

    name

--- src/Header.js
+++ src/Header.js
@@ -31,9 +31,11 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <Link to="/login">
+        <Link to={!user && "/login"}>
           <div onClick={handleAuthentication} className="header__option">
-            <span className="header__optionLineOne">Hello Guest</span>
+            <span className="header__optionLineOne">
+              Hello {!user ? "Guest" : user.email}
+            </span>
             <span className="header__optionLineTwo">
               {user ? "Sign In" : "Sign Out"}
             </span>
--- src/Login.js
+++ src/Login.js
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { auth } from "./firebase";
 
